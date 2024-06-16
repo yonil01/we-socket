00e5e0682aa874110706852409b1e5cd70248e03
yonil01 <54485570+yonil01@users.noreply.github.com>
2024-06-16 11:57:38 -0500

    Add files via upload
    new

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+
   ws.on('message', (message) => {
     console.log(`Received message: ${message}`);
-    ws.send(`Echo: ${message}`);
+    // Enviar el mensaje a todos los clientes conectados
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
   });
+
   ws.on('close', () => {
     console.log('Client disconnected');
   });
